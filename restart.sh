b8baae7c9859df49164209aa713846c729b22f5b
Douglas Fernandes <douglasf.filho@gmail.com>
2020-09-01 09:48:02 -0300

    add: messages

--- restart.sh
+++ restart.sh
@@ -11,3 +11,10 @@ docker-compose up -d
 
 cd /home/douglasffilho/workspace/BNW/kibanas/kibana-qa/
 docker-compose up -d
+
+echo 'Pleases wait...'
+for i in 0..40; do
+    sleep(1000);
+done
+
+echo 'you can use kibana locally ihuuuuu!'
